bc514945039f8c8fc7f480ad111fecbcd35dfdcb
Your Name <you@example.com>
2024-08-26 19:00:02 +0500

    update bucket origin

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "cloudcore0072"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = ">= 1.6.3"
