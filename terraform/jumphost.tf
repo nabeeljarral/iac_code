2b10b3b1a96a7bd7012348b873f3d2485f54beed
nabeeljarral <40709574+nabeeljarral@users.noreply.github.com>
2024-08-26 19:38:43 +0500

    Update jumphost.tf

--- terraform/jumphost.tf
+++ terraform/jumphost.tf
@@ -1,5 +1,4 @@
-
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
